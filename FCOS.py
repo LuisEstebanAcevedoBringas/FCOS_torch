55ddc222c1a2a4c01f24a1adf8b0fcaa125edc31
Luis Bringas <acevedo.bringas.luis.esteban@gmail.com>
2022-06-07 20:45:26 -0500

    Update.

--- FCOS.py
+++ FCOS.py
@@ -28,6 +28,7 @@ def FCOS_TransferLearning(num_classes):
     torch.nn.init.constant_(cls_logits.bias, -math.log((1 - 0.01) / 0.01))
 
     model.head.classification_head.cls_logits = cls_logits
+
     print(model)
     return model
 
@@ -36,11 +37,17 @@ def FCOS_FineTuning(num_classes):
 
     model = torchvision.models.detection.fcos_resnet50_fpn(pretrained=True)
     
-    in_features = det_utils.retrieve_out_channels(model.backbone, (300, 300))
+    in_features = model.head.classification_head.conv[0].in_channels
+    num_anchors = model.head.classification_head.num_anchors
+    model.head.classification_head.num_classes = num_classes
 
-    anchors = model.anchor_generator.num_anchors_per_location()
-    
-    model.head.classification_head = FCOSClassificationHead(in_features, anchors, num_classes)
+    out_channels = 256
+
+    cls_logits = torch.nn.Conv2d(out_channels, num_anchors * num_classes, kernel_size = 3, stride=1, padding=1)
+    torch.nn.init.normal_(cls_logits.weight, std=0.01)
+    torch.nn.init.constant_(cls_logits.bias, -math.log((1 - 0.01) / 0.01))
+
+    model.head.classification_head.cls_logits = cls_logits
 
     print(model)
     return model
@@ -50,12 +57,17 @@ def FCOS_FromScratch(num_classes):
 
     model = torchvision.models.detection.fcos_resnet50_fpn(pretrained=False)
     
-    in_features = det_utils.retrieve_out_channels(model.backbone, (300, 300))
+    in_features = model.head.classification_head.conv[0].in_channels
+    num_anchors = model.head.classification_head.num_anchors
+    model.head.classification_head.num_classes = num_classes
 
-    anchors = model.anchor_generator.num_anchors_per_location()
+    out_channels = 256
 
-    model.head.classification_head = FCOSClassificationHead(in_features, anchors, num_classes)
+    cls_logits = torch.nn.Conv2d(out_channels, num_anchors * num_classes, kernel_size = 3, stride=1, padding=1)
+    torch.nn.init.normal_(cls_logits.weight, std=0.01)
+    torch.nn.init.constant_(cls_logits.bias, -math.log((1 - 0.01) / 0.01))
+
+    model.head.classification_head.cls_logits = cls_logits
 
     print(model)
     return model
-
