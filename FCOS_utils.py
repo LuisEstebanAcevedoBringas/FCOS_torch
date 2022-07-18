cfb231db1a5217d5db96d0447ef03171a9f70c34
Luis Bringas <acevedo.bringas.luis.esteban@gmail.com>
2022-07-17 23:19:52 -0500

    Update

--- FCOS_utils.py
+++ FCOS_utils.py
@@ -23,7 +23,7 @@ rev_label_map = {v: k for k, v in label_map.items()}  # Inverse mapping
 
 # Color map for bounding boxes of detected objects from https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
 #distinct_colors = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#d2f53c', '#fabebe', '#008080', '#000080', '#aa6e28', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#e6beff', '#808080', '#FFFFFF']
-distinct_colors = ['#ffffff',' #e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231']
+distinct_colors = ['#ffffff', '#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231']
 
 label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())}
 
